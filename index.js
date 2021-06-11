1798c9b6b8150dd554e9b7c417376bd0d0881e25
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-06-11 13:56:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Write your code here
 const n1 = 2;
 const n2 = 31;
-const multiply = n1 * n2;
+const multiply = () => {
+  return n1 * n2;
+};
 const random = Math.floor( Math.random() * 100 ) + 1;
 const n3 = 16;
 const n4 = 6;
