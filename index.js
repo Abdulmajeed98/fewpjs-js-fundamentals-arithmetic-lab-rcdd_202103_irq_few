ddfad69056b8e443ed9aefde65540d4cf0117931
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-06-11 13:52:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here
+const n1 = 2;
+const n2 = 31;
+const multiply = n1 * n2;
+const random = Math.floor( Math.random() * 100 ) + 1;
+const n3 = 16;
+const n4 = 6;
+const mod = num3 % num4;
+const max = Math.max( 5, 10, 15, 20 );
